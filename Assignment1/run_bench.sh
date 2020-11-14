888a4cd4099ef8f70e72590d83c5f0e207aacce2
florianw <f.wuensche@campus.tu-berlin.de>
2020-11-14 17:37:12 +0100

    reupload

--- Assignment1/run_bench.sh
+++ Assignment1/run_bench.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# sysbench for cpu benchmarking, grep the line with events per second
-result=$(sysbench  cpu --max-time=60 run | grep "events per second")
 
-# cut the string, first split on :, take the second part, then split again on whitespace, such that you only retrieve the number
-cputime=$(echo $result| cut -d":"   -f2 | cut -d" " -f2)
+# prepare random access read disk sysbench, 1gb total file size, mode --> random read, extra-flags --> direct access, no caching, file-num--> use only one file instead of default
+prepare=$(sysbench fileio --file-total-size=1GB --file-test-mode=rndrd --file-extra-flags=direct --file-num=1 prepare)
+
+# prepare sequential disk read acces benchmarking, 1gb total file size, sequential read mode, direct access, use only one file
+prepareseqrd=$(sysbench fileio --file-total-size=1GB --file-test-mode=seqrd --file-extra-flags=direct --file-num=1 prepare)
+
 
 
 # get epoch timestamp in seconds
 timestamp=$(date +%s)
 
+# sysbench for cpu benchmarking, grep the line with events per second
+result=$(sysbench  cpu --max-time=60 run | grep "events per second")
+
+# cut the string, first split on :, take the second part, then split again on whitespace, such that you only retrieve the number
+cputime=$(echo $result| cut -d":"   -f2 | cut -d" " -f2)
 
 # sysbench the memory, 4kb block size, 100tb total, for 60 seconds. grep line with transferred, since here is the result 
 resultmem=$(sysbench memory --memory-oper=read --memory-block-size=4kb --memory-total-size=100TB --max-time=60 run | grep "transferred")
@@ -18,21 +25,13 @@ resultmem=$(sysbench memory --memory-oper=read --memory-block-size=4kb --memory-
 rm=$( echo $resultmem | cut -d"(" -f2)
 rm2=$(echo ${rm%?} | cut -d" " -f1)
 
-
-# prepare random access read disk sysbench, 1gb total file size, mode --> random read, extra-flags --> direct access, no caching, file-num--> use only one file instead of default
-prepare=$(sysbench fileio --file-total-size=1GB --file-test-mode=rndrd --file-extra-flags=direct --file-num=1 prepare)
-
 # run the rnd read test for max 60 seconds, grep result
 rndr=$(sysbench fileio --file-total-size=1GB --file-test-mode=rndrd --file-extra-flags=direct --file-num=1 --max-time=60 run | grep "read, MiB/s:")
 # string manipulation
 rn=$(echo $rndr | cut -d" " -f3)
-# echo $rn
-
-# prepare sequential disk read acces benchmarking, 1gb total file size, sequential read mode, direct access, use only one file
-prepareseqrd=$(sysbench fileio --file-total-size=1GB --file-test-mode=seqrd --file-extra-flags=direct --file-num=1 prepare)
 
 # run the benchmarking and grep result line
-seqr=$(sysbench fileio --file-total-size=1GB --file-test-mode=seqrd --file-extra-flags=direct --file-num=1 --max-time=3 run | grep "read, MiB/s:")
+seqr=$(sysbench fileio --file-total-size=1GB --file-test-mode=seqrd --file-extra-flags=direct --file-num=1 --max-time=60 run | grep "read, MiB/s:")
 # string manipulation
 seqrn=$(echo $seqr | cut -d" " -f3)
 
@@ -40,3 +39,7 @@ seqrn=$(echo $seqr | cut -d" " -f3)
 final="${timestamp},${cputime},${rm2},${rn},${seqrn}"
 # print them and enjoy :)
 echo $final
+
+# crontab command
+# get PATH with pwd command from terminal
+# */30 * * * * /PATH/TO/run_bench.sh >> /PATH/TO/aws_result.csv

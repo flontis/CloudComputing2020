f0e18886482d5c995e5dc68bb18dcb95dc3c48ac
flontis <39523379+flontis@users.noreply.github.com>
2020-11-21 15:06:20 +0100

    added explanation for -1 parameter at port arguments

--- Assignment1/spinup-aws.sh
+++ Assignment1/spinup-aws.sh
@@ -9,17 +9,18 @@ aws ec2 describe-vpcs
 # import previously generated key pair, fileb is used to interpret the file as binary
 aws ec2 import-key-pair --key-name "cc20" --public-key-material fileb://~/.ssh/id_rsa.pub
 
-# create a security group for the VPC. Get the VPC-ID via "aws ec2 describe-vpcs"
+# create a security group with name and description
 aws ec2 create-security-group --group-name CC20SecGroup --description "SecurityGroup for CC20"
 
-# gain access for all incoming ssh traffic. Use the --group-name of the security group created before.
+# gain access for all incoming ssh traffic. Use the --group-name of the security group created before
 aws ec2 authorize-security-group-ingress --group-name CC20SecGroup --protocol tcp --port 22 --cidr 0.0.0.0/0
 
 # allow all incoming IMCP traffic from every ipv4 and ipv6 address, again by using the --group-name
+# parameter -1 for port arguments denotes all ICMP codes
 aws ec2 authorize-security-group-ingress --group-name CC20SecGroup --ip-permissions IpProtocol=icmpv6,FromPort=-1,ToPort=-1,Ipv6Ranges='[{CidrIpv6=::/0}]'
 aws ec2 authorize-security-group-ingress --group-name CC20SecGroup --ip-permissions IpProtocol=icmp,FromPort=-1,ToPort=-1,IpRanges='[{CidrIp=0.0.0.0/0}]'
 
-# run the instance with the corresponding key, type, security group and image.
+# run the instance with the corresponding key, type, security group and image
 aws ec2 run-instances --key-name "cc20" --instance-type "t2.large" --security-groups "CC20SecGroup" --image-id "ami-00ddb0e5626798373"
 
 # resize the volume, get volume-id via "aws ec2 describe-volumes"

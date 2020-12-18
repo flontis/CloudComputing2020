03c52c11eb0e822fa5d2998e7e2b5b2b647aeb6b
flontis <39523379+flontis@users.noreply.github.com>
2020-12-18 17:19:14 +0100

    Update exec_benchmark.sh

--- assignment3/exec_benchmark.sh
+++ assignment3/exec_benchmark.sh
@@ -5,22 +5,26 @@ echo "time,cpu,mem,diskRand,diskSeq,fork,uplink" > docker-results.csv
 echo "time,cpu,mem,diskRand,diskSeq,fork,uplink" > kvm-results.csv
 echo "time,cpu,mem,diskRand,diskSeq,fork,uplink" > qemu-results.csv
 
+echo "################ Running benchmark on host ################"
 for run in {1..10}
 do
 	./benchmark.sh >> native-results.csv
 done
 
+echo "################ Building Docker container ################"
 sudo docker build -t benchmark_container .
 
+echo "################ Running benchmark on docker ################"
 for run in {1..10}
 do
 	sudo docker container run benchmark_container >> docker-results.csv
 done
 
 # for KVM
+echo "################ Running benchmark on KVM ################"
 sshpass -p 'DeepMind' scp -P 2222 forkbench.c florian@localhost:/home/florian/
 sshpass -p 'DeepMind' scp -P 2222 benchmark.sh florian@localhost:/home/florian/
-sshpass -p 'DeepMind' ssh -p 2222 florian@localhost
+
 
 for run in {1..10}
 do
@@ -29,9 +33,11 @@ done
 
 
 # for Qemu, adjust the port when invoking qemu
+
+echo "################ Running benchmark on Qemu ################"
 sshpass -p 'DeepMind' scp -P 2223 forkbench.c florian@localhost:/home/florian/
 sshpass -p 'DeepMind' scp -P 2223 benchmark.sh florian@localhost:/home/florian/
-sshpass -p 'DeepMind' ssh -p 2223 florian@localhost
+
 
 for run in {1..10}
 do
